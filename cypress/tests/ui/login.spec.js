19351cf34afe3211db5d4a53f74c621c6632355f
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2025-12-19 23:54:11 -0300

    Add beforeEach

--- cypress/tests/ui/login.spec.js
+++ cypress/tests/ui/login.spec.js
@@ -3,14 +3,17 @@ import LoginPage from '../../pages/loginPage';
 const loginPage = new LoginPage();
 
 describe('Login RWA', () => {
+  beforeEach(() => {
+    cy.visit('/signin');
+  });
+
   it('Deve fazer login com um usuário válido', () => {
-    loginPage.accessLoginPage();
-    loginPage.loginWithAnyUser('Heath93', 's3cret');    
+    loginPage.loginWithAnyUser('Heath93', 's3cret');
     cy.location("pathname").should("equal", "/");
+    cy.get("[data-test='transaction-list']").should('be.visible');
   });
 
   it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
-    loginPage.accessLoginPage();
     loginPage.loginWithAnyUser('Heath93', '123456');
     loginPage.checkAccessInvalid();
   });
