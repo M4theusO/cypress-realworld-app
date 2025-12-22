4e956e1c8eddb94c3938cc4b01a6f944954ab2d2
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2025-12-22 17:38:33 -0300

    Add selectorList

--- cypress/tests/e2e/user.spec.js
+++ cypress/tests/e2e/user.spec.js
@@ -3,28 +3,38 @@ import LoginPage from '../../pages/loginPage';
 const loginPage = new LoginPage();
 const credenciais = require('../../credenciais.json');
 
+const selectorList = {
+  firstNameInput: '[data-test="signup-first-name"]',
+  lastNameInput: '[data-test="signup-last-name"]',
+  usernameInput: '[data-test="signup-username"]',
+  passwordInput: '[data-test="signup-password"]',
+  confirmPasswordInput: '[data-test="signup-confirmPassword"]',
+  submitButton: '[data-test="signup-submit"]',
+  nextButton: '[data-test="user-onboarding-next"]'
+};
+
 describe('User register', () => {
   beforeEach(() => {
     cy.visit('/signup');
   });
 
   it('Deve registrar um novo usuário com informações válidas', () => {
-    cy.get('[data-test="signup-first-name"]').type(credenciais.signup.newUser.firstName);
-    cy.get('[data-test="signup-last-name"]').type(credenciais.signup.newUser.lastName);
-    cy.get('[data-test="signup-username"]').type(credenciais.signup.newUser.username);
-    cy.get('[data-test="signup-password"]').type(credenciais.signup.newUser.password);
-    cy.get('[data-test="signup-confirmPassword"]').type(credenciais.signup.newUser.password);
-    cy.get('[data-test="signup-submit"]').click();
+    cy.get(selectorList.firstNameInput).type(credenciais.signup.newUser.firstName);
+    cy.get(selectorList.lastNameInput).type(credenciais.signup.newUser.lastName);
+    cy.get(selectorList.usernameInput).type(credenciais.signup.newUser.username);
+    cy.get(selectorList.passwordInput).type(credenciais.signup.newUser.password);
+    cy.get(selectorList.confirmPasswordInput).type(credenciais.signup.newUser.password);
+    cy.get(selectorList.submitButton).click();
     loginPage.loginWithAnyUser(credenciais.signup.newUser.username, credenciais.signup.newUser.password);
     cy.location("pathname").should("equal", "/");
-    cy.get('[data-test="user-onboarding-next"]').should('be.visible');
+    cy.get(selectorList.nextButton).should('be.visible');
   });
 
   it('Deve exibir mensagens de erro ao tentar registrar um novo usuário sem preencher todas as informações obrigatórias', () => {
-    cy.get('[data-test="signup-first-name"]').type(credenciais.signup.newUser.firstName);
-    cy.get('[data-test="signup-last-name"]').type(credenciais.signup.newUser.lastName);
-    cy.get('[data-test="signup-username"]').type(credenciais.signup.newUser.username);
-    cy.get('[data-test="signup-password"]').type(credenciais.signup.newUser.password);
-    cy.get('[data-test="signup-submit"]').should('be.disabled');
+    cy.get(selectorList.firstNameInput).type(credenciais.signup.newUser.firstName);
+    cy.get(selectorList.lastNameInput).type(credenciais.signup.newUser.lastName);
+    cy.get(selectorList.usernameInput).type(credenciais.signup.newUser.username);
+    cy.get(selectorList.passwordInput).type(credenciais.signup.newUser.password);
+    cy.get(selectorList.submitButton).should('be.disabled');
   });
 });
